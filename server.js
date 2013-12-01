2d1fd057755a416312190267ca92c4d8cfcb3fc1
Equan Pr <eq@junwatu.com>
2013-12-01 22:37:52 +0700

    fix serial port harus di open dulu.

--- server.js
+++ server.js
@@ -2,37 +2,46 @@
  * list dependencies
  */
 var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
-	serialPort = require('serialport').SerialPort;
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    serialPort = require('serialport').SerialPort,
+
+    /*
+     * Serial Port Setup
+     */
+    portName = '/dev/ttyACM0',
+    //Array to hold the values read from the port
+    readData = "",
+    sp = new serialPort(portName, {
+        baudRate: 57600,
+        dataBits: 8,
+        parity: 'none',
+        stopBits: 1,
+        flowControl: false,
+    });
 
 
-
-/*
- * Serial Port Setup
- */
-
-var portName = '/dev/ttyACM0';
-var readData = ""; //Array to hold the values read from the port
-
-var sp = new serialPort(portName, {
-    baudRate : 57600,
-    dataBits : 8,
-    parity : 'none',
-    stopBits: 1,
-    flowControl : false,
-});
-
-
-
-io.sockets.on('connection', function(socket, debug){
-    if(debug == false){
+io.sockets.on('connection', function (socket, debug) {
+    if (debug == false) {
         socketServer.set('log level', 1);
     }
-    socket.on('button', function(data){
-        console.log("data from client: "+data.lampstatus);
-        //serialPort.write(data);
+    socket.on('button', function (data) {
+
+        var status = data.lampstatus;
+        console.log("data from client: " + status);
+
+        sp.open(function () {
+            console.log('open');
+            sp.on('data', function(data) {
+                console.log('data received: ' + data);
+            });
+
+            sp.write(status, function(err, result){
+                if(err) console.log('[ERROR] '+err);
+                console.log(result);
+            });
+        });
     })
 });
 
@@ -43,23 +52,17 @@ io.sockets.on('connection', function(socket, debug){
 
 
 // serve static files from index
-app.configure(function(){
-	app.use(express.static(__dirname + '/'));
+app.configure(function () {
+    app.use(express.static(__dirname + '/'));
 });
 
 // respon to web GET request on index.html
-app.get('/', function (req, res){
-	res.sendfile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
 });
 
 
 server.listen(3000);
 console.log('listen on port 3000')
-
-
-
-
-
-
 
 
